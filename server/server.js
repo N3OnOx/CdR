87950ea2b50d0c078b7211dd8872433472c44135
Alejandro <alexsanchezarbeit@outlook.com>
2018-09-27 13:19:10 +0200

    Segundo commit

--- server/server.js
+++ server/server.js
@@ -1,20 +1,40 @@
-const express = require('express');
+require('./config/config');
 
+const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const publicPath = path.resolve(__dirname, '../public');
+
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
+// Public path
 app.use(express.static(publicPath));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 
-app.listen(port, (err) => {
+// Configuración global de rutas
+app.use(require('./routes/index'));
 
-    if (err) throw new Error(err);
 
-    console.log(`Servidor corriendo en puerto ${ port }`);
 
+mongoose.connect(process.env.URLDB, (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
+
+});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
 });
