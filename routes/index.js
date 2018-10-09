8e0c5eb9f418fa4b5f4cff4f0bb04df6785bb040
Alejandro Sanchez Jimenez <alexsanchezarbeit@outlook.com>
2018-10-09 17:34:00 +0200

    Fix

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Family = require('../models/family')
 
 // Homepage
 router.get('/', ensureAuthenticated, function(req, res) {
@@ -10,10 +11,32 @@ router.get('/', ensureAuthenticated, function(req, res) {
 
 // Web2 Page
 router.get('/web2', ensureAuthenticated, function(req, res) {
-    res.render('web2', {
-        tiempo: 10,
-        familyId: req.user.family
+
+    let id = req.user.family;
+    Family.findById(id, (err, familiaDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!familiaDB) {
+            return res.status(400).json({
+                ok: false,
+                message: 'No existe familia en la bbdd'
+            });
+        }
+        let nivel = familiaDB.construction[0][3].length;
+        res.render('web2', {
+            tiempo: 10,
+            familyId: req.user.family,
+            maxLevel: nivel,
+            level: familiaDB.construction[0][2]
+        });
     });
+
 });
 
 function ensureAuthenticated(req, res, next) {
